51351e1c114c489e46067eebc5f6bd90573f94f3
dario-nihil <dario.nihil@gmail.com>
2024-03-06 17:21:07 +0100

    started implementation of screen navigation using Navigator widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,16 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
+  @override
+  State<App> createState() {
+    return _AppState();
+  }
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
